c2684f8552cd1b699646ef2603d492f2aae9fb80
CharlesWu16 <635285686@qq.com>
2018-08-17 21:37:58 +0800

    javaday01

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package day01;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
-		System.out.println("林嘉怡英文名是fat chloe");
+		System.out.println("林嘉怡英文名是 chloe");
 	}
 }
